999adeb1a2ac1864e9d88d7b5d595a4bc57ef92d
Maxwell Lanzkron-Tamarazo <maxwell.lt@live.com>
2020-02-13 11:59:19 -0500

    Improve appearance of purchase page
    Drop experimental Markdown support
    Remove legacy JS and JSON files
    Make head tags more consistent across pages

--- src/main/java/maxwell_lt/socialmediaproject/dto/Pack.java
+++ src/main/java/maxwell_lt/socialmediaproject/dto/Pack.java
@@ -3,18 +3,20 @@ package maxwell_lt.socialmediaproject.dto;
 import java.math.BigDecimal;
 
 public enum Pack {
-    SMALL(100, new BigDecimal("0.99")),
-    MEDIUM(525, new BigDecimal("4.99")), // 5% bonus!
-    LARGE(1100, new BigDecimal("9.99")), // 10% bonus!
-    SUPER(5750, new BigDecimal("49.99")), // 15% bonus!
-    EXTREME(12500, new BigDecimal("99.99")); // 25% bonus! Wow!
+    SMALL(100, new BigDecimal("0.99"), "Basic Pack"),
+    MEDIUM(525, new BigDecimal("4.99"), "Medium Pack\n5% more likes!"), // 5% bonus!
+    LARGE(1100, new BigDecimal("9.99"), "Large Pack\n10% more likes!"), // 10% bonus!
+    SUPER(5750, new BigDecimal("49.99"), "Super Pack\n15% more likes!"), // 15% bonus!
+    EXTREME(12500, new BigDecimal("99.99"), "BEST VALUE\nExtreme Pack\n25% more likes!"); // 25% bonus! Wow!
 
     private int size;
     private BigDecimal price;
+    private String description;
 
-    Pack(int size, BigDecimal price) {
+    Pack(int size, BigDecimal price, String description) {
         this.size = size;
         this.price = price;
+        this.description = description;
     }
 
     public int getSize() {
@@ -23,5 +25,9 @@ public enum Pack {
 
     public BigDecimal getPrice() {
         return price;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
