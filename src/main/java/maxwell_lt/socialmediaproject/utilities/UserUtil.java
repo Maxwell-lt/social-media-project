7ebb9d008ebd5e688c69da6e216d5c1906937f21
Maxwell Lanzkron-Tamarazo <maxwell.lt@live.com>
2020-03-02 09:40:37 -0500

    Fix HTTP 405 on account management endpoints

--- src/main/java/maxwell_lt/socialmediaproject/utilities/UserUtil.java
+++ src/main/java/maxwell_lt/socialmediaproject/utilities/UserUtil.java
@@ -1,5 +1,6 @@
 package maxwell_lt.socialmediaproject.utilities;
 
+import com.nulabinc.zxcvbn.Zxcvbn;
 import maxwell_lt.socialmediaproject.dto.UserPrincipal;
 import maxwell_lt.socialmediaproject.dto.UserRegistrationForm;
 import maxwell_lt.socialmediaproject.entity.User;
@@ -9,6 +10,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.Instant;
@@ -19,11 +22,13 @@ public class UserUtil {
 
     private PasswordEncoder passwordEncoder;
     private UserService userService;
+    private Zxcvbn zxcvbn;
 
     @Autowired
-    public UserUtil(PasswordEncoder passwordEncoder, UserService userService) {
+    public UserUtil(PasswordEncoder passwordEncoder, UserService userService, Zxcvbn zxcvbn) {
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
+        this.zxcvbn = zxcvbn;
     }
 
     public User createUserFromRegistrationForm(UserRegistrationForm userRegistrationForm) {
@@ -45,5 +50,34 @@ public class UserUtil {
             return userService.getUserById(userId);
         }
         return Optional.empty();
+    }
+
+    public boolean validateUsername(String username) {
+        return (username != null &&
+                !username.isEmpty() &&
+                username.length() <= 50 &&
+                !userService.getUserByUsername(username).isPresent());
+    }
+
+    public boolean validateEmail(String email) {
+        boolean validAddress;
+        try {
+            new InternetAddress(email).validate();
+            validAddress = true;
+        } catch (AddressException e) {
+            validAddress = false;
+        }
+        return (email != null &&
+                !email.isEmpty() &&
+                email.length() <= 254 &&
+                validAddress &&
+                !userService.getUserByEmail(email).isPresent());
+    }
+
+    public boolean validatePassword(String password) {
+        return (password != null &&
+                !password.isEmpty() &&
+                password.length() <= 48 &&
+                zxcvbn.measure(password).getScore() > 2);
     }
 }
--- src/main/java/maxwell_lt/socialmediaproject/view/AccountController.java
+++ src/main/java/maxwell_lt/socialmediaproject/view/AccountController.java
@@ -11,16 +11,12 @@ import maxwell_lt.socialmediaproject.service.PostlikesService;
 import maxwell_lt.socialmediaproject.service.UserService;
 import maxwell_lt.socialmediaproject.utilities.PostUtil;
 import maxwell_lt.socialmediaproject.utilities.UserUtil;
-import maxwell_lt.socialmediaproject.validator.EmailNotExist;
-import maxwell_lt.socialmediaproject.validator.PasswordStrength;
-import maxwell_lt.socialmediaproject.validator.UsernameNotExist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,11 +24,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -90,7 +81,7 @@ public class AccountController {
 
     private static Map<String, Object> getStatusMap(boolean status, Object message) {
         Map<String, Object> result = new HashMap<>();
-        result.put("success", Boolean.toString(status));
+        result.put("success", status);
         result.put("message", message);
         return result;
     }
@@ -144,16 +135,10 @@ public class AccountController {
     @PutMapping("/manage_account/password")
     public @ResponseBody
     Map<String, Object> updatePassword(@RequestParam("oldpass") String oldPassword,
-                                       @RequestParam("newpass")
-                                       @NotNull
-                                       @NotEmpty(message = "Password cannot be empty!")
-                                       @Size(max = 48)
-                                       @PasswordStrength
-                                       @Valid String newPassword,
-                                       BindingResult result) {
+                                       @RequestParam("newpass") String newPassword) {
         User user = userUtil.getCurrentUser().orElseThrow(NotAuthenticatedException::new);
-        if (result.hasErrors()) {
-            return getStatusMap(false, result.getModel());
+        if (!userUtil.validatePassword(newPassword)) {
+            return getStatusMap(false);
         }
         if (passwordEncoder.matches(oldPassword, user.getPassword())) {
             userService.updatePassword(user, passwordEncoder.encode(newPassword));
@@ -164,14 +149,8 @@ public class AccountController {
 
     @PutMapping("/manage_account/username")
     public @ResponseBody
-    Map<String, Object> updateUsername(@RequestParam("username")
-                                       @NotNull
-                                       @NotEmpty(message = "Username cannot be empty!")
-                                       @Size(max = 50, message = "Size must be between 1 and 50 characters!")
-                                       @UsernameNotExist
-                                       @Valid String username,
-                                       BindingResult result) {
-        if (result.hasErrors()) {
+    Map<String, Object> updateUsername(@RequestParam("username") String username) {
+        if (!userUtil.validateUsername(username)) {
             return getStatusMap(false);
         }
         int userId = userUtil.getCurrentUser().orElseThrow(NotAuthenticatedException::new).getId();
@@ -180,14 +159,8 @@ public class AccountController {
 
     @PutMapping("/manage_account/email")
     public @ResponseBody
-    Map<String, Object> updateEmail(@RequestParam("email") @NotNull
-                                    @NotEmpty(message = "Email cannot be empty!")
-                                    @Size(max = 254)
-                                    @Email
-                                    @EmailNotExist
-                                    @Valid String email,
-                                    BindingResult result) {
-        if (result.hasErrors()) {
+    Map<String, Object> updateEmail(@RequestParam("email") String email) {
+        if (!userUtil.validateEmail(email)) {
             return getStatusMap(false);
         }
         int userId = userUtil.getCurrentUser().orElseThrow(NotAuthenticatedException::new).getId();
